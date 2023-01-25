6f82b74e68434341692c7f63da91b6f027df3c16
Mesih Grmay <mesihgrmay@yahoo.com>
2023-01-25 19:49:38 +0000

    Task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,9 +5,9 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((res) => (
-      res.map((o) => ({
-        status: o.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      res.map((val) => ({
+        status: val.status,
+        value: val.status === 'fulfilled' ? val.value : String(val.reason),
       }))
     ));
 }
