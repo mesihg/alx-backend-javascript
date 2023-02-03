456cea416c64378bf4b155bed86e3fb2902333ec
Mesih Grmay <mesihgrmay@yahoo.com>
2023-02-03 11:47:13 +0000

    Task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
-export default function getListStudentIds(studentList) {
-  if (studentList instanceof Array) {
-    return studentList.map(student => student.id);
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
   }
-  return []
+  return [];
 }
