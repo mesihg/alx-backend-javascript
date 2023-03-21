e994ae4376b2d0066adbe0fac7d4a7131b046f35
Mesih Grmay <mesihgrmay@yahoo.com>
2023-03-21 19:05:27 +0000

    Task 6

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const PORT = 1245;
+
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
+
+module.exports = app;
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
