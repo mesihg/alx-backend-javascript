262d1f63630079823fdc0f973f81d4952019e887
Mesih Grmay <mesihgrmay@yahoo.com>
2023-03-23 14:17:30 +0000

    Task 6

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,11 +2,11 @@ const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('getPaymentTokenFromAPI(success)', () => {
+  it('getPaymentTokenFromAPI(success)', (done) => {
     getPaymentTokenFromAPI(true)
-	 .then((res) => {
-	   expect(res).to.deep.equal({ data: 'Successful response from the API' });
-           done();
-     });
+      .then((res) => {
+        expect(res).to.deep.equal({data: 'Successful response from the API'});
+        done();
+      });
   });
 });
